69dbfbd47d5f3af4253ac7cc09af981b4d8f1c65
cristian <cristian.torres19@hotmail.com>
2020-10-12 16:06:47 -0400

    test

---
+++ src/components/header/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles.scss'
+
+
+const Header = () => {
+    return (
+       <>
+       <div className="header">
+           <div className="warp">
+               <div className="logo">
+           <h1>Testing</h1>
+
+               </div>
+           </div>
+       </div>
+       </>
+    )
+}
+
+export default Header;
+
+
